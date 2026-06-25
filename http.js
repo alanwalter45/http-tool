1fb926f2eddf0247185a6db921cd08a213048b20
AlanWalterMD <alanwalter45@gmail.com>
2026-06-25 13:30:13 -0400

    0.0.8

--- http.js
+++ http.js
@@ -1,96 +1,94 @@
 import Axios from 'axios';
 
+let auth = {};
 let path = '';
 
+function isEmpty(object) {
+  return Object.keys(object).length == 0;
+}
+
 export default {
-	getAuth(nameLocalStorage) {
-		return {
-			headers: {
-				Authorization: `Bearer ${localStorage.getItem(nameLocalStorage) == null
-					? ''
-					: localStorage.getItem(nameLocalStorage).replace(/"/g, '')}`
-			}
-		};
-	},
-	setPathBase: function (pathBase, prefix='') {
-		path = `${pathBase}/${prefix?prefix:''}`;
-	},
-	get: function (url, auth) {
-		if (auth) {
-			return Axios.get(`${path}${url}`, auth);
-		}
-		return Axios.get(`${path}${url}`);
-	},
-	getBlob: function (url, auth) {
-		if (auth) {
-			auth.responseType = 'blob';
-			return Axios.get(`${path}${url}`, auth);
-		}
-		return Axios.get(`${path}${url}`, {
-			responseType: 'blob'
-		});
-	},
-	post: function (url, obj, auth) {
-		if (auth) {
-			return Axios.post(`${path}${url}`, JSON.parse(JSON.stringify(obj)), auth);
-		}
-		return Axios.post(`${path}${url}`, JSON.parse(JSON.stringify(obj)));
-	},
-	postBlob: function (url, obj, auth) {
-		if (auth) {
-			auth.responseType = 'blob';
-			return Axios.post(`${path}${url}`, JSON.parse(JSON.stringify(obj)), auth);
-		}
-		return Axios.post(`${path}${url}`, JSON.parse(JSON.stringify(obj)), { responseType: 'blob' });
-	},
-	postStr: function (url, str, auth) {
-		if (auth) {
-			return Axios.post(
-				`${path}${url}`,
-				str,
-				{
-					headers: {
-						'Content-Type': 'text/plain'
-					}
-				},
-				auth
-			);
-		}
-		return Axios.post(`${path}${url}`, str, {
-			headers: {
-				'Content-Type': 'text/plain'
-			}
-		});
-	},
-	delete: function (url, obj, auth) {
-		if (auth) {
-			return Axios.delete(`${path}${url}/${obj.id}`, auth);
-		}
-		return Axios.delete(`${path}${url}/${obj.id}`);
-	},
-	put: function (url, obj, auth) {
-		if (auth) {
-			return Axios.put(`${path}${url}/${obj.id}`, JSON.parse(JSON.stringify(obj)), auth);
-		}
-		return Axios.put(`${path}${url}/${obj.id}`, JSON.parse(JSON.stringify(obj)));
-	},
-	postFile: function (url, formData, auth) {
-		if (auth) {
-			return Axios.post(
-				`${path}${url}`,
-				formData,
-				{
-					headers: {
-						'Content-Type': 'multipart/form-data'
-					}
-				},
-				auth
-			);
-		}
-		return Axios.post(`${path}${url}`, formData, {
-			headers: {
-				'Content-Type': 'multipart/form-data'
-			}
-		});
-	}
+  setAuth(nameLocalStorage = '') {
+    if (nameLocalStorage == '') {
+      auth = {};
+      return;
+    }
+    auth = {
+      headers: {
+        Authorization: `Bearer ${
+          localStorage.getItem(nameLocalStorage) == null
+            ? ''
+            : localStorage.getItem(nameLocalStorage).replace(/"/g, '')
+        }`,
+      },
+    };
+  },
+  setPathBase: function (pathBase, prefix = '') {
+    path = `${pathBase}/${prefix ? prefix : ''}`;
+  },
+  get: function (url) {
+    return Axios.get(`${path}${url}`, !isEmpty(auth) ? auth : undefined);
+  },
+  getBlob: function (url) {
+    if (!isEmpty(auth)) {
+      auth.responseType = 'blob';
+      return Axios.get(`${path}${url}`, auth);
+    }
+    return Axios.get(`${path}${url}`, {
+      responseType: 'blob',
+    });
+  },
+  post: function (url, obj) {
+    return Axios.post(
+      `${path}${url}`,
+      JSON.parse(JSON.stringify(obj)),
+      !isEmpty(auth) ? auth : undefined,
+    );
+  },
+  postBlob: function (url, obj) {
+    if (!isEmpty(auth)) {
+      auth.responseType = 'blob';
+      return Axios.post(`${path}${url}`, JSON.parse(JSON.stringify(obj)), auth);
+    }
+    return Axios.post(`${path}${url}`, JSON.parse(JSON.stringify(obj)), {
+      responseType: 'blob',
+    });
+  },
+  postStr: function (url, str) {
+    return Axios.post(
+      `${path}${url}`,
+      str,
+      {
+        headers: {
+          'Content-Type': 'text/plain',
+        },
+      },
+      !isEmpty(auth) ? auth : undefined,
+    );
+  },
+  delete: function (url, obj) {
+    return Axios.delete(
+      `${path}${url}/${obj.id}`,
+      !isEmpty(auth) ? auth : undefined,
+    );
+  },
+  put: function (url, obj) {
+    return Axios.put(
+      `${path}${url}/${obj.id}`,
+      JSON.parse(JSON.stringify(obj)),
+      !isEmpty(auth) ? auth : undefined,
+    );
+  },
+  postFile: function (url, formData) {
+    return Axios.post(
+      `${path}${url}`,
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      },
+      !isEmpty(auth) ? auth : undefined,
+    );
+  },
 };
